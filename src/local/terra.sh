e761b49a08cfcfa0802c9341709c47e66ee89cea
Jake Hickenlooper <jake@weboftomorrow.com>
2022-08-12 08:12:29 -0600

    Set project environment to match workspace

--- src/local/terra.sh
+++ src/local/terra.sh
@@ -13,6 +13,7 @@ fi
 project_dir="$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")"
 terraform_infra_dir="$project_dir/src/terraform/010-infra"
 terraform_chillbox_dir="$project_dir/src/terraform/020-chillbox"
+chillbox_artifact_version="$(cat "$project_dir/src/chillbox/VERSION")"
 
 SKIP_UPLOAD="${SKIP_UPLOAD:-n}"
 
@@ -31,6 +32,34 @@ chillbox_build_artifact_vars_file="$chillbox_state_home/build-artifacts-vars"
 dist_sites_dir="$chillbox_state_home/sites"
 site_domains_file="$chillbox_state_home/site_domains.auto.tfvars.json"
 ssh_keys_file="$chillbox_state_home/developer-public-ssh-keys.auto.tfvars.json"
+chillbox_instance_and_environment_file="$chillbox_state_home/chillbox-instance-and-environment.auto.tfvars.json"
+if [ "$WORKSPACE" = "development" ]; then
+  environment="Development"
+  project_environment="Development"
+elif [ "$WORKSPACE" = "test" ]; then
+  environment="Test"
+  project_environment="Staging"
+elif [ "$WORKSPACE" = "acceptance" ]; then
+  environment="Acceptance"
+  project_environment="Staging"
+elif [ "$WORKSPACE" = "production" ]; then
+  environment="Production"
+  project_environment="Production"
+else
+  echo "ERROR $0: Unhandled WORKSPACE value."
+  exit 1
+fi
+jq --null-input \
+  --arg jq_cb_instance "$CHILLBOX_INSTANCE" \
+  --arg jq_cb_environment "$environment" \
+  --arg jq_cb_project_environment "$project_environment" \
+  --arg jq_cb_project_version "$chillbox_artifact_version" \
+  '{
+    chillbox_instance: $jq_cb_instance,
+    environment: $jq_cb_environment,
+    project_environment: $jq_cb_project_environment,
+    project_version: $jq_cb_project_version,
+  }' > "$chillbox_instance_and_environment_file"
 
 export INFRA_IMAGE="chillbox-terraform-010-infra:latest"
 export INFRA_CONTAINER="chillbox-terraform-010-infra-$CHILLBOX_INSTANCE-$WORKSPACE"
@@ -91,6 +120,7 @@ docker_run_infra_container() {
     --mount "type=volume,src=chillbox-terraform-var-lib--$CHILLBOX_INSTANCE-${WORKSPACE},dst=/var/lib/doterra,readonly=false" \
     --mount "type=volume,src=chillbox-${INFRA_CONTAINER}-var-lib--$CHILLBOX_INSTANCE-${WORKSPACE},dst=/var/lib/terraform-010-infra,readonly=false" \
     --mount "type=bind,src=${TERRAFORM_INFRA_PRIVATE_AUTO_TFVARS_FILE},dst=/usr/local/src/chillbox-terraform/private.auto.tfvars" \
+    --mount "type=bind,src=${chillbox_instance_and_environment_file},dst=/usr/local/src/chillbox-terraform/chillbox-instance-and-environment.auto.tfvars.json,readonly=true" \
     --entrypoint="" \
     "$INFRA_IMAGE" "$@"
 }
@@ -149,6 +179,7 @@ docker_run_chillbox_container() {
     --mount "type=bind,src=$chillbox_state_home/$SITES_MANIFEST,dst=/usr/local/src/chillbox-terraform/dist/$SITES_MANIFEST,readonly=true" \
     --mount "type=bind,src=$chillbox_state_home/$SITES_ARTIFACT,dst=/usr/local/src/chillbox-terraform/dist/$SITES_ARTIFACT,readonly=true" \
     --mount "type=bind,src=$dist_sites_dir,dst=/usr/local/src/chillbox-terraform/dist/sites,readonly=true" \
+    --mount "type=bind,src=${chillbox_instance_and_environment_file},dst=/usr/local/src/chillbox-terraform/chillbox-instance-and-environment.auto.tfvars.json,readonly=true" \
     --entrypoint="" \
     "$TERRAFORM_CHILLBOX_IMAGE" "$@"
 }
--- tests/fixtures/example-chillbox-config/acceptance/terraform-010-infra/example-private.auto.tfvars
+++ tests/fixtures/example-chillbox-config/acceptance/terraform-010-infra/example-private.auto.tfvars
@@ -1,2 +1,4 @@
 # It is not recommended to store any secrets in a private.auto.tfvars file.
 # These should only be considered as parameters to use when deploying.
+
+project_description = "Infrastructure for hosting websites that use Chill."
