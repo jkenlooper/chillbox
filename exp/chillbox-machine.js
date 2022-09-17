f70b4ba7f70c8c42d11b8d30d80e64599639afbc
Jake Hickenlooper <jake@weboftomorrow.com>
2022-09-17 07:16:27 -0600

    Add WIP statechart from stately.ai

--- exp/chillbox-machine.js
+++ exp/chillbox-machine.js
@@ -63,3 +63,156 @@ const chillboxMachine = createMachine({
 
 export { chillboxMachine };
 
+
+// https://stately.ai/registry/editor/share/1b1a3595-5a82-4bfe-ba32-ba3b856e59f2
+const exportedStateChart =
+{
+  "id": "on local box",
+  "initial": "Running chillbox script",
+  "description": "Executing the doit.sh experimental script.",
+  "states": {
+    "Running chillbox script": {
+      "initial": "Get Opts",
+      "states": {
+        "Get Opts": {
+          "on": {
+            "done": {
+              "target": "show environment"
+            }
+          }
+        },
+        "show environment": {
+          "on": {
+            "done": {
+              "target": "check args and environment vars"
+            }
+          }
+        },
+        "check args and environment vars": {
+          "on": {
+            "done": {
+              "target": "check for required commands"
+            }
+          }
+        },
+        "check for required commands": {
+          "on": {
+            "done": {
+              "target": "init and source chillbox config"
+            }
+          }
+        },
+        "init and source chillbox config": {
+          "on": {
+            "done": {
+              "target": "create example site tar gz"
+            }
+          }
+        },
+        "create example site tar gz": {
+          "on": {
+            "done": {
+              "target": "validate environment vars"
+            }
+          }
+        },
+        "validate environment vars": {
+          "on": {
+            "done": {
+              "target": "build artifacts"
+            }
+          }
+        },
+        "build artifacts": {
+          "on": {
+            "done": {
+              "target": "verify built artifacts"
+            }
+          }
+        },
+        "verify built artifacts": {
+          "on": {
+            "done": {
+              "target": "generate site domains"
+            }
+          }
+        },
+        "generate site domains": {
+          "on": {
+            "done": {
+              "target": "check sub command"
+            }
+          }
+        },
+        "check sub command": {
+          "on": {
+            "Event 2": [
+              {
+                "cond": "interactive",
+                "target": "update ssh keys auto tfvars"
+              },
+              {
+                "cond": "plan",
+                "target": "update ssh keys auto tfvars"
+              },
+              {
+                "cond": "apply",
+                "target": "update ssh keys auto tfvars"
+              },
+              {
+                "cond": "destroy",
+                "target": "update ssh keys auto tfvars"
+              },
+              {
+                "cond": "clean",
+                "target": "execute clean script"
+              },
+              {
+                "cond": "pull",
+                "target": "execute pull terraform script"
+              },
+              {
+                "cond": "push",
+                "target": "execute push terraform script"
+              },
+              {
+                "cond": "secrets",
+                "target": "execute secrets script"
+              }
+            ]
+          }
+        },
+        "update ssh keys auto tfvars": {
+          "on": {
+            "done": {
+              "target": "execute terra script"
+            }
+          }
+        },
+        "execute terra script": {},
+        "execute clean script": {},
+        "execute pull terraform script": {},
+        "execute push terraform script": {},
+        "execute secrets script": {}
+      }
+    },
+    "Abort": {
+      "type": "final",
+      "description": "Did not finish the process."
+    },
+    "Exited script": {
+      "type": "final"
+    }
+  },
+  "on": {
+    "SIGHUP": {
+      "target": ".Abort"
+    },
+    "SIGINT": {
+      "target": ".Abort"
+    },
+    "SIGQUIT": {
+      "target": ".Abort"
+    }
+  }
+}
