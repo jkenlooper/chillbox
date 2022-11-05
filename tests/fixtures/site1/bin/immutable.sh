901e6d328fe4295dc50f3bb435d8868488ab4432
Jake Hickenlooper <jake@weboftomorrow.com>
2022-11-05 11:34:17 -0600

    Add immutable-example to tests/fixtures/site1
    This is a more complete example for the immutable example.

--- tests/fixtures/site1/bin/immutable.sh
+++ tests/fixtures/site1/bin/immutable.sh
@@ -2,33 +2,81 @@
 
 set -o errexit
 
-slugname=site1
-
+script_name="$(basename "$0")"
 projectdir="$(dirname "$(dirname "$(realpath "$0")")")"
 
-# Wouldn't normally use the Makefile for a hash, but this is just for testing.
-immutable_example_hash="$(md5sum "$projectdir/Makefile" | cut -d' ' -f1)"
+usage() {
+  cat <<HERE
 
-# archive file path should be absolute
-archive="$(realpath "$1")"
-echo "$archive" | grep -q "\.tar\.gz$" || (echo "First arg should be an archive file ending with .tar.gz" && exit 1)
+Create the archive file from the immutable dirs.
 
-tmpdir="$(mktemp -d)"
-mkdir -p "$tmpdir/$slugname"
+Usage:
+  $script_name -h
+  $script_name -s <slugname> -t <archive_file> <directory> [<directory>..]
 
-mkdir -p "$tmpdir/$slugname/immutable-example/$immutable_example_hash"
-find "$projectdir/" -type f | sort >  "$tmpdir/$slugname/immutable-example/$immutable_example_hash/man.txt"
-echo "file 1 example" > "$tmpdir/$slugname/immutable-example/$immutable_example_hash/file1.txt"
-echo "file 2 example" > "$tmpdir/$slugname/immutable-example/$immutable_example_hash/file2.txt"
+Options:
+  -h                  Show this help message.
 
-archive_dir="$(dirname "$archive")"
-mkdir -p "$archive_dir"
-tar c \
-  -C "$tmpdir" \
-  -h \
-  -z \
-  -f "${archive}" \
-  "$slugname"
+  -s <slugname>       Set the slugname.
 
-# Clean up
-rm -rf "${tmpdir}"
+  -t <archive_file>   Set the archive tar.gz file to create.
+
+Args:
+  <directory>   Create the archive file from the provided directories which
+                should each have their own Makefile that creates files in a dist
+                directory.
+
+HERE
+}
+
+create_archive() {
+  archive="$(realpath "$archive_file")"
+  echo "$archive" | grep -q "\.tar\.gz$" || (echo "ERROR $script_name: The archive file provided ($archive_file) should end with .tar.gz" >&2 && exit 1)
+
+  tmpdir="$(mktemp -d)"
+  mkdir -p "$tmpdir/$slugname"
+
+  for directory in "$@"; do
+    test -n "$directory" || continue
+    directory_full_path="$(realpath "$directory")"
+    test -d "$directory_full_path" || (echo "ERROR $script_name: The provided directory ($directory) is not a directory at $directory_full_path" >&2 && exit 1)
+    directory_basename="$(basename "$directory_full_path")"
+    hash_string="$(make --silent -C "$directory_full_path" inspect.HASH)"
+    mkdir -p "$tmpdir/$slugname/$directory_basename/$hash_string"
+    printf "%s" "$hash_string" > "$tmpdir/$slugname/$directory_basename/hash.txt"
+    make -C "$directory_full_path"
+    find "$directory_full_path/dist/" -depth -mindepth 1 -maxdepth 1 -exec cp -R {} "$tmpdir/$slugname/$directory_basename/$hash_string/" \;
+  done
+
+  archive_dir="$(dirname "$archive")"
+  mkdir -p "$archive_dir"
+  tar c \
+    -C "$tmpdir" \
+    -h \
+    -z \
+    -f "${archive}" \
+    "$slugname"
+
+  # Clean up
+  rm -rf "${tmpdir}"
+}
+
+slugname=""
+archive_file=""
+
+while getopts "hs:t:" OPTION ; do
+  case "$OPTION" in
+    h) usage
+       exit 0 ;;
+    s) slugname=$OPTARG ;;
+    t) archive_file=$OPTARG ;;
+    ?) usage
+       exit 1 ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+test -n "$slugname" || (echo "ERROR $script_name: No slugname set." >&2 && usage && exit 1)
+test -n "$archive_file" || (echo "ERROR $script_name: No archive_file set." >&2 && usage && exit 1)
+
+create_archive "$@"
