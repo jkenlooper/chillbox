3f21f50be21b36b06030388d4140614740b9e811
Jake Hickenlooper <jake@weboftomorrow.com>
2022-11-29 07:58:05 -0700

    Add chillbox-trigger-update service

--- src/chillbox/bin/update.sh
+++ src/chillbox/bin/update.sh
@@ -12,7 +12,19 @@ set -o errexit
 mkdir -p /usr/local/src/
 cd /usr/local/src/
 
-/etc/chillbox/bin/site-init.sh
 
-/etc/chillbox/bin/reload-templates.sh
+now_iso_seconds="$(date -Iseconds)"
+chillbox_update_log="/var/log/chillbox-update/$now_iso_seconds.log"
+chillbox_update_log_dir="$(dirname "$chillbox_update_log")"
+mkdir -p "$chillbox_update_log_dir"
+touch "$chillbox_update_log"
+chmod 0640 "$chillbox_update_log"
+chown root:ansibledev "$chillbox_update_log"
+
+/etc/chillbox/bin/site-init.sh >> "$chillbox_update_log" 2>&1 || (cat "$chillbox_update_log" && exit 1)
+
+/etc/chillbox/bin/reload-templates.sh >> "$chillbox_update_log" 2>&1 || (cat "$chillbox_update_log" && exit 1)
+nginx -t >> "$chillbox_update_log" 2>&1 || (cat "$chillbox_update_log" && exit 1)
 nginx -t && rc-service nginx reload
+
+cat "$chillbox_init_log"
